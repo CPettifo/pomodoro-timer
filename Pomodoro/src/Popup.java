49f1d2fa1a93693dac63cea5877ca89d7c670862
CPettifo <cpettifo@myune.edu.au>
2023-07-28 14:24:40 +0200

    create stub popup class

--- Pomodoro/src/Popup.java
+++ Pomodoro/src/Popup.java
@@ -2,4 +2,21 @@
  * This class is used for the popups that will be displayed to the user to let them know how much time has elapsed
  */
 public class Popup {
+    private String message;
+    private String popupColour;
+
+    public Popup (String message, String popupColour){
+        this.message = message;
+        this.popupColour = popupColour;
+        displayPopup();
+    }
+
+    /**
+     * Placeholder function, for now it will just display the message in the console, eventually I would like it to be
+     * a pop-up notification from the System Tray
+     */
+    private void displayPopup(){
+        System.out.println(this.message);
+        System.out.println("The colour of this message would be: " + popupColour);
+    }
 }
