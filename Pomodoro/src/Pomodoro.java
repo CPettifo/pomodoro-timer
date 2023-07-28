d6f5d623d2e7c0a4a9c2930c9397636598a0d5be
CPettifo <cpettifo@myune.edu.au>
2023-07-28 14:37:10 +0200

    successfully test how to use timers

--- Pomodoro/src/Pomodoro.java
+++ Pomodoro/src/Pomodoro.java
@@ -1,6 +1,18 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * This is the Main class which will call all other classes as necessary
  */
 public class Pomodoro {
+    public static void main(String[] args){
+        Timer timer = new Timer();
+
+        Popup task = new Popup();
+
+        task.setPopup("This is a popup", "#FFFFFF");
+
+        timer.schedule(task, 200, 5000);
+    }
 
 }
