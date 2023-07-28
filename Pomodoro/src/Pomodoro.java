1d1e84ba3ea7d941a5e70b9d0d202e4c3568dc2d
CPettifo <cpettifo@myune.edu.au>
2023-07-28 14:55:43 +0200

    create example functions for each of the separate time periods

--- Pomodoro/src/Pomodoro.java
+++ Pomodoro/src/Pomodoro.java
@@ -5,14 +5,34 @@ import java.util.TimerTask;
  * This is the Main class which will call all other classes as necessary
  */
 public class Pomodoro {
+    Timer timer = new Timer();
+    static Popup start = new Popup(); // The TimerTask-like object for the very first popup
+    Popup shortPause = new Popup(); // The Object for the first break
+    Popup longPause = new Popup();
+    Popup afterBreak = new Popup(); //The Popup to display after a break has completed
+
+
     public static void main(String[] args){
-        Timer timer = new Timer();
+        // Set the start popup and run it immediately
+        start.setPopup("Good work on choosing to be productive today, next break in 25 seconds", "#00FF00");
+        start.run();
 
-        Popup task = new Popup();
+    }
 
-        task.setPopup("This is a popup", "#FFFFFF");
 
-        timer.schedule(task, 200, 5000);
+    public void shortBreak(){
+        shortPause.setPopup("Good work on working so hard! Have a little fiver", "#445F22");
+        timer.schedule(shortPause, 200, 5000);
+    }
+
+    public void longBreak(){
+        longPause.setPopup("Awesome! Time for a bigger break now", "#FFFFFF");
+        timer.schedule(longPause, 200, 30000);
+    }
+
+    public void startWork(){
+        afterBreak.setPopup("Alright, time to get back to it, see you after you work a bit more", "#545454");
+        timer.schedule(afterBreak, 200, 25000);
     }
 
 }
