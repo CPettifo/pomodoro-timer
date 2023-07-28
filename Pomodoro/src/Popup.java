9845f25f2865d23c74a8106fd7399726885488b0
CPettifo <cpettifo@myune.edu.au>
2023-07-28 14:28:51 +0200

    change Popup class to be an instance of TimerTask class

--- Pomodoro/src/Popup.java
+++ Pomodoro/src/Popup.java
@@ -1,21 +1,22 @@
+import java.util.TimerTask;
+
 /**
  * This class is used for the popups that will be displayed to the user to let them know how much time has elapsed
  */
-public class Popup {
+public class Popup extends TimerTask{
     private String message;
     private String popupColour;
 
     public Popup (String message, String popupColour){
         this.message = message;
         this.popupColour = popupColour;
-        displayPopup();
     }
 
     /**
      * Placeholder function, for now it will just display the message in the console, eventually I would like it to be
      * a pop-up notification from the System Tray
      */
-    private void displayPopup(){
+    public void run(){
         System.out.println(this.message);
         System.out.println("The colour of this message would be: " + popupColour);
     }
